01860a207b1abc2df4cb6fb7b96a1cac47088dfb
nikzen <nicolasivanbianchi@outlook.com>
2022-06-25 03:02:56 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let cuentaRegresiva = 10
+
+console.log("Comienza la cuenta regresiva para el despegue...!")
+
+while(cuentaRegresiva != 0){
+    console.log("Cuenta Regresiva: " + cuentaRegresiva + "...")
+    // cuentaRegresiva = cuentaRegresiva - 1
+    cuentaRegresiva -= 1
+} 
+
+console.log("Despegue")
