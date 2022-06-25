519fbafda19d826255bb4b5169179e5862f91ecd
nikzen <nicolasivanbianchi@outlook.com>
2022-06-25 03:26:49 -0300

    add if else

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-let cuentaRegresiva = 10
+// let cuentaRegresiva = 10
 
-console.log("Comienza la cuenta regresiva para el despegue...!")
+// console.log("Comienza la cuenta regresiva para el despegue...!")
 
-while(cuentaRegresiva != 0){
-    console.log("Cuenta Regresiva: " + cuentaRegresiva + "...")
-    // cuentaRegresiva = cuentaRegresiva - 1
-    cuentaRegresiva -= 1
-} 
+// while(cuentaRegresiva != 0){
+//     console.log("Cuenta Regresiva: " + cuentaRegresiva + "...")
+//     // cuentaRegresiva = cuentaRegresiva - 1
+//     cuentaRegresiva -= 1
+// } 
 
-console.log("Despegue")
+// console.log("Despegue")
+
+let nico = 24;
+let mati = 24;
+let ingresaConEntrada = true;
+
+if((ingresaConEntrada) && (nico >= 18 || mati >= 18)){
+    console.log("Entran con la entrada");
+}else{
+    console.log("No entran");
+}
