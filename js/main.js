f901029dab7d292b50ed5e8888922b7597baf4da
nikzen <nicolasivanbianchi@outlook.com>
2022-07-12 21:00:54 -0300

    add: array concat

--- js/main.js
+++ js/main.js
@@ -9,3 +9,8 @@ function onRangeUpdate() {
     let bulletPosition = (rangeSlider.value / rangeSlider.max);
     rangeBullet.style.left = (bulletPosition * 578) + "px";
 }
+
+const pages1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const pages2 = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const allPages = pages1.concat(pages2);
+console.log(allPages);
